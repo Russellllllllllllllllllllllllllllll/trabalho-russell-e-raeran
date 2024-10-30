740de468ee255a8e64f729760ff7c260227ab421
Ruselll <140611116+Russellllllllllllllllllllllllllllll@users.noreply.github.com>
2024-10-30 09:45:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Matemática',
+    'quantos segundos se tem em 10 anos?',
+    'Dez anos têm 315.360.000 segundos'
+)
+
+criaCartao(
+    'Geografia',
+    'Como a tectônica de placas influencia os padrões climáticos e a biodiversidade em diferentes regiões do planeta ao longo do tempo geológico?',
+    'A tectônica de placas altera o clima e cria barreiras naturais, mudando habitats e promovendo biodiversidade.'
+)
+
+criaCartao(
+    'Literatura',
+    'qual a maior palavra da lingua portuguesa?',
+    '"pneumoultramicroscopicossilicovulcanoconiótico'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que vem após a morte?',
+    'O que vem após a morte é um mistério. Diferentes culturas e religiões oferecem explicações variadas, como a ideia de vida após a morte, reencarnação ou um estado de paz. A ciência, por sua vez, foca na cessação das funções biológicas. No fim, a resposta depende das crenças pessoais e filosóficas de cada um.'
+)
+criaCartao(
+    'Biologia',
+    'Porque as galinhas não voam?',
+    'Galinhas não voam porque são pesadas e têm asas curtas, adaptadas para pequenos saltos, não para voos longos.'
+    )
+    criaCartao(
+        'Quimica',
+        'Por que o hélio faz a voz parecer mais aguda quando inalado?',
+        'O hélio é menos denso que o ar, o que aumenta a velocidade das ondas sonoras e faz a voz parecer mais aguda ao ser inalado.'
+    )
